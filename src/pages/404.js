db207f70bf6f72456aa87917eb782711458f2323
Ivan Ivanusic <ivan.ivanusic@hotmail.com>
2020-10-15 13:48:20 +0200

    project clean up

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,11 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+const IndexPage = () => (
+  <main>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>This is were we will add content</p>
+  </main>
 )
 
-export default NotFoundPage
+export default IndexPage
