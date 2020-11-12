890edb5078f9bb9e8126065726afe281a41e0743
Ivan Ivanusic <ivan.ivanusic@hotmail.com>
2020-11-12 11:03:49 +0100

    adding ImagePool

--- src/components/GalleryContainer/index.js
+++ src/components/GalleryContainer/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-
+ 
+import ImagePool from '../ImagePool'
+ 
 import styles from './style.module.css'
-
-const GalleryContainer = ({ title }) => (
+ 
+const GalleryContainer = ({title}) => (
     <section className={styles.galleryContainer}>
         <h1>{title}</h1>
-        <div>IMAGES</div>
+        <ImagePool />
     </section>
 )
-
+ 
 export default GalleryContainer
