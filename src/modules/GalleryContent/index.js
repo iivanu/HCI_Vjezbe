1e7e80365229d7da6761a5f97edf65259c7dea82
Ivan Ivanusic <ivan.ivanusic@hotmail.com>
2020-11-12 11:08:23 +0100

    Upgrading GalleryContainer to accept bounds and style

--- src/modules/GalleryContent/index.js
+++ src/modules/GalleryContent/index.js
@@ -3,12 +3,12 @@ import React from 'react'
 import styles from './style.module.css'
 import GalleryContainer from '../../components/GalleryContainer'
  
-const GalleryContinent = () => (
+const GalleryContent = () => (
     <main className={styles.galleryContent}>
-        <GalleryContainer title="Amazing exterior" />
-        <GalleryContainer title="Relaxing sauna" />
-        <GalleryContainer title="Modern interior" />
+        <GalleryContainer title="Amazing exterior" start={0} end={4} isGray />
+        <GalleryContainer title="Relaxing sauna" start={4} end={8} />
+        <GalleryContainer title="Modern interior" start={8} end={24} isGray />
     </main>
 )
  
-export default GalleryContinent
+export default GalleryContent
